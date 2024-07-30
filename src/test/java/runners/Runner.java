105ccfb548bbfd4c144b17238d2d2e190384833d
cihankaranfil44@hotmail.com <cihankaranfil44@hotmail.com>
2024-07-30 10:57:13 +0300

    30.07.2024 "v3"

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         monochrome = true,
         features = "./src/test/resources/features",
         glue = {"stepDefination", "hooks"},
-        tags = "@crm",
+        tags = "@Superadmin",
         dryRun = false
 )
 public class
